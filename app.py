a92fd320fcf0753d6999a99c2b0bfb1ce3a56e5b
riliya <riliyatop@gmail.com>
2026-04-21 15:43:15 +0300

    Test OTP

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static', 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'webp'}
 
 app.config.update(
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_PORT=os.getenv("MAIL_PORT"),
-    MAIL_USE_TLS=True,
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_DEFAULT_SENDER='no-reply@mpit.su'
+    MAIL_SERVER="localhost",
+    MAIL_PORT = 25,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = None,
+    MAIL_PASSWORD = None
 )
 
 mail = Mail(app)
